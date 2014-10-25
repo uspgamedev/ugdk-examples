44f2ea4055de8ee4c6500468a214eecc27ae1913
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2014-10-25 21:14:27 -0200

    Update keyboard-box example.

--- keyboard-box/keyboard-box.cc
+++ keyboard-box/keyboard-box.cc
@@ -3,25 +3,36 @@
 #include <ugdk/input/module.h>
 #include <ugdk/input/events.h>
 #include <ugdk/input/scancode.h>
-#include <ugdk/action/entity.h>
-#include <ugdk/graphic/node.h>
 #include <ugdk/graphic/canvas.h>
-#include <ugdk/graphic/drawable/texturedrectangle.h>
+#include <ugdk/graphic/vertexdata.h>
 #include <ugdk/graphic/module.h>
+#include <ugdk/structure/vertex.h>
 
 using namespace ugdk;
 
-class Rectangle : public action::Entity {
+namespace {
+    const static float RECTANGLE_SIZE = 50.0f;
+}
+
+void QuitOnEscape(const ugdk::input::KeyPressedEvent& ev) {
+    if (ev.scancode == ugdk::input::Scancode::ESCAPE)
+        ugdk::system::CurrentScene().Finish();
+}
+
+class Rectangle {
   public:
     Rectangle() 
-      : velocity_(500.0), 
-        drawable_(new graphic::TexturedRectangle(graphic::manager()->white_texture(), math::Vector2D(50.0, 50.0))) {
-    }
-    ~Rectangle() {
-        delete drawable_;
+      : velocity_(500.0)
+      , vertexdata_(4, sizeof(structure::VertexXYUV), true)
+    {
+        graphic::VertexData::Mapper mapper(vertexdata_);
+        mapper.Get<structure::VertexXYUV>(0)->set_xyuv(          0.0f,           0.0f, 0.0f, 0.0f);
+        mapper.Get<structure::VertexXYUV>(1)->set_xyuv(          0.0f, RECTANGLE_SIZE, 0.0f, 1.0f);
+        mapper.Get<structure::VertexXYUV>(2)->set_xyuv(RECTANGLE_SIZE,           0.0f, 1.0f, 0.0f);
+        mapper.Get<structure::VertexXYUV>(3)->set_xyuv(RECTANGLE_SIZE, RECTANGLE_SIZE, 1.0f, 1.0f);
     }
 
-    void Update(double dt) override {
+    void Update(double dt) {
         auto manager = input::manager();
         if(manager->keyboard().IsDown(input::Scancode::A))
             MoveLeft(dt);
@@ -46,34 +57,39 @@ class Rectangle : public action::Entity {
         position_.y += dt*velocity_;
     }
 
-    const math::Vector2D& position() const { return position_; }
-    graphic::TexturedRectangle* drawable() { return drawable_; }
+    void Render(graphic::Canvas& canvas) const {
+        canvas.PushAndCompose(graphic::Geometry(position_));
+        canvas.SendVertexData(vertexdata_, graphic::VertexType::VERTEX, offsetof(structure::VertexXYUV, x));
+        canvas.SendVertexData(vertexdata_, graphic::VertexType::TEXTURE, offsetof(structure::VertexXYUV, u));
+        auto unit = graphic::manager()->ReserveTextureUnit(graphic::manager()->white_texture());
+        canvas.SendUniform("drawable_texture", unit);
+        canvas.DrawArrays(graphic::DrawMode::TRIANGLE_STRIP(), 0, 4);
+        canvas.PopGeometry();
+    }
 
   private:
-    math::Vector2D position_;
     double velocity_;
-    graphic::TexturedRectangle* drawable_;
+    math::Vector2D position_;
+    graphic::VertexData vertexdata_;
 };
 
 int main(int argc, char* argv[]) {
     system::Initialize();
 
-    action::Scene* scene = new action::Scene();
-    Rectangle* r = new Rectangle;
+    auto scene = ugdk::MakeUnique<ugdk::action::Scene>();
+    scene->event_handler().AddListener(QuitOnEscape);
 
-    scene->event_handler().AddListener<input::KeyPressedEvent>(
-        [scene](const input::KeyPressedEvent& ev) -> void {
-            if(ev.scancode == input::Scancode::ESCAPE)
-                scene->Finish();
+    {
+        auto r = std::make_shared<Rectangle>();
+        scene->AddTask([r](double dt) {
+            r->Update(dt);
         });
-    scene->AddEntity(r);
-    scene->set_render_function([r](graphic::Canvas& canvas) {
-        canvas.PushAndCompose(graphic::Geometry(r->position()));
-        r->drawable()->Draw(canvas);
-        canvas.PopGeometry();
-    });
+        scene->set_render_function([r](graphic::Canvas& canvas) {
+            r->Render(canvas);
+        });
+    }
+    system::PushScene(std::move(scene));
 
-    system::PushScene(scene);
     system::Run();
     system::Release();
     return 0;
