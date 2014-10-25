b4cc4e8e34c08948c2144b782b8ec2738f6ae572
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2014-10-25 21:41:25 -0200

    Fix draggable-box.

--- draggable-box/draggable-box.cc
+++ draggable-box/draggable-box.cc
@@ -1,45 +1,44 @@
 #include <ugdk/system/engine.h>
 #include <ugdk/action/scene.h>
 #include <ugdk/input/events.h>
-#include <ugdk/action/entity.h>
-#include <ugdk/graphic/canvas.h>
-#include <ugdk/graphic/drawable/texturedrectangle.h>
-#include <ugdk/graphic/module.h>
-#include <ugdk/system/config.h>
-#include <ugdk/desktop/module.h>
 #include <ugdk/desktop/window.h>
+#include <ugdk/graphic/canvas.h>
+#include <ugdk/graphic/module.h>
+#include <ugdk/ui/drawable/texturedrectangle.h>
+
+#include <memory>
 
 using namespace ugdk;
 
-int main(int argc, char *argv[]) {
+namespace {
+    const static math::Vector2D box_size(50.0, 50.0);
+}
 
+int main(int argc, char *argv[]) {
     system::Initialize();
 
-    math::Vector2D box_size(50.0, 50.0);
+    auto scene = ugdk::MakeUnique<ugdk::action::Scene>();
+    math::Vector2D box_position;
+    {
+        auto rect = std::make_shared<ui::TexturedRectangle>(graphic::manager()->white_texture(), box_size);
+        rect->set_hotspot(ui::HookPoint::CENTER);
 
-    auto window_size = desktop::manager()->primary_window()->size();
-    auto canvas_size = graphic::manager()->canvas()->size();
-    math::Vector2D window_to_canvas(canvas_size.x / window_size.x, canvas_size.y / window_size.y);
-    graphic::Geometry box_position;
-
-    action::Scene* scene = new action::Scene();
-    
-    auto r = new graphic::TexturedRectangle(graphic::manager()->white_texture(), box_size);
-    r->set_hotspot(graphic::Drawable::CENTER);
-    scene->set_render_function([&](graphic::Canvas& canvas) {
-        canvas.PushAndCompose(box_position);
-        r->Draw(canvas);
-        canvas.PopGeometry();
-    });
-    scene->event_handler().AddListener<input::MouseMotionEvent>(
-        [&](const input::MouseMotionEvent& ev) {
-            box_position.ChangeOffset(math::Vector2D(ev.position).Scale(window_to_canvas));
+        scene->event_handler().AddListener<input::MouseMotionEvent>([rect, &box_position](const input::MouseMotionEvent& ev) {
+            auto window = ev.window.lock();
+            box_position.x = double(ev.position.x) / window->size().x;
+            box_position.y = double(ev.position.y) / window->size().y;
+        });
+        scene->set_render_function([rect, &box_position](graphic::Canvas& canvas) {
+            math::Vector2D canvas_position = box_position.Scale(canvas.size());
+            canvas.PushAndCompose(graphic::Geometry(canvas_position));
+            rect->Draw(canvas);
+            canvas.PopGeometry();
         });
 
-    system::PushScene(scene);
-    system::Run();
-    delete r;
-    
+    }
+    system::PushScene(std::move(scene));
+
+    system::Run();    
     system::Release();
     return 0;
 }
