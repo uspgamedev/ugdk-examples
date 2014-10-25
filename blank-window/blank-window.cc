8316c2c10e43873d8cb5365dc83f263e7b5b0d39
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2014-10-25 20:46:38 -0200

    Fix blank window example.

--- blank-window/blank-window.cc
+++ blank-window/blank-window.cc
@@ -1,16 +1,20 @@
 #include <ugdk/system/engine.h>
 #include <ugdk/action/scene.h>
 #include <ugdk/input/events.h>
+#include <ugdk/system/compatibility.h>
+
+void QuitOnEscape(const ugdk::input::KeyPressedEvent& ev) {
+    if (ev.scancode == ugdk::input::Scancode::ESCAPE)
+        ugdk::system::CurrentScene().Finish();
+}
 
 int main(int argc, char* argv[]) {
-    assert(ugdk::system::Initialize());
-    ugdk::action::Scene* ourscene = new ugdk::action::Scene;
-    ourscene->event_handler().AddListener<ugdk::input::KeyPressedEvent>(
-        [ourscene](const ugdk::input::KeyPressedEvent& ev) {
-            if(ev.scancode == ugdk::input::Scancode::ESCAPE)
-                ourscene->Finish();
-        });
-    ugdk::system::PushScene(ourscene);
+    ugdk::system::Initialize();
+
+    auto ourscene = ugdk::MakeUnique<ugdk::action::Scene>();
+    ourscene->event_handler().AddListener(QuitOnEscape);
+    ugdk::system::PushScene(std::move(ourscene));
+
     ugdk::system::Run();
     ugdk::system::Release();
     return 0;
