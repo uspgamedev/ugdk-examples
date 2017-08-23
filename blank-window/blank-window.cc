8ada90e6c91b048378ab40714c0e4ce6f79986f0
Kazuo256 <kazuo256@gmail.com>
2017-08-23 16:44:41 -0300

    Use C++14 and adequate to current API
    Signed off by: @josealvim
    Signed off by: @icemage144

--- blank-window/blank-window.cc
+++ blank-window/blank-window.cc
@@ -1,7 +1,10 @@
+
 #include <ugdk/system/engine.h>
 #include <ugdk/action/scene.h>
 #include <ugdk/input/events.h>
 #include <ugdk/system/compatibility.h>
+
+#include <memory>
 
 void QuitOnEscape(const ugdk::input::KeyPressedEvent& ev) {
     if (ev.scancode == ugdk::input::Scancode::ESCAPE)
@@ -9,13 +12,17 @@ void QuitOnEscape(const ugdk::input::KeyPressedEvent& ev) {
 }
 
 int main(int argc, char* argv[]) {
-    ugdk::system::Initialize();
+    using namespace ugdk;
+    system::Initialize();
+    system::FunctionListener<input::KeyPressedEvent> listener(QuitOnEscape);
 
-    auto ourscene = ugdk::MakeUnique<ugdk::action::Scene>();
-    ourscene->event_handler().AddListener(QuitOnEscape);
-    ugdk::system::PushScene(std::move(ourscene));
+    auto ourscene = std::make_unique<action::Scene>();
 
-    ugdk::system::Run();
-    ugdk::system::Release();
+    ourscene->event_handler().AddListener(listener);
+
+    system::PushScene(std::move(ourscene));
+
+    system::Run();
+    system::Release();
     return 0;
 }
