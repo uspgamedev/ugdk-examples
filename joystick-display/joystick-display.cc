5f84daac84381eac633c4443bae7e24df88bffe3
Kazuo256 <kazuo256@gmail.com>
2017-10-02 16:27:14 -0300

    Add background for joystick display
    Signed off by: @josealvim
    Signed off by: @icemage144

--- joystick-display/joystick-display.cc
+++ joystick-display/joystick-display.cc
@@ -35,29 +35,52 @@ namespace {
 
 using namespace ugdk;
 using math::Vector2D;
+using structure::Color;
 using std::shared_ptr;
 
-const Vector2D  BOX_SIZE(50.0, 50.0);
 const double    SPEED = 500.0;
+
+struct JoystickDisplay;
+
+std::vector<JoystickDisplay> displays;
 
 struct VertexXYUV {
     F32 x, y, u, v;
 };
 
 struct Box {
-    Vector2D pos;
-    structure::Color color;
+    Vector2D size;
+    Color color;
     graphic::GLTexture *tex;
     graphic::VertexData vtx;
 };
 
+Box makeBox(const Vector2D& size, const Color& color) {
+    // Box data
+    Box box = {
+        size,
+        color,
+        graphic::manager().white_texture(),
+        std::move(graphic::VertexData(4u, sizeof(VertexXYUV), false))
+    };
+    // Map box vertices
+    {
+        graphic::VertexData::Mapper mapper(box.vtx, false);
+        mapper.Get<VertexXYUV>(0u) = {.0f, .0f, .0f, .0f};
+        mapper.Get<VertexXYUV>(1u) = {.0f, 1.f, .0f, 1.f};
+        mapper.Get<VertexXYUV>(2u) = {1.f, .0f, 1.f, .0f};
+        mapper.Get<VertexXYUV>(3u) = {1.f, 1.f, 1.f, 1.f};
+    }
+    return std::move(box);
+}
+
 graphic::Canvas& operator<<(graphic::Canvas& canvas, const Box& box) {
-    auto &pos = box.pos;
+    auto &size = box.size;
     auto &texture = box.tex;
     auto &vertex_data = box.vtx;
     auto &color = box.color;
 
-    canvas.PushAndCompose(math::Geometry(pos - BOX_SIZE/2));
+    canvas.PushAndCompose(math::Geometry(Vector2D(), size));
     canvas.PushAndCompose(color);
     
     graphic::TextureUnit unit = graphic::manager().ReserveTextureUnit(texture);
@@ -76,16 +99,27 @@ graphic::Canvas& operator<<(graphic::Canvas& canvas, const Box& box) {
 struct JoystickDisplay {
     shared_ptr<input::Joystick> joystick;
     shared_ptr<text::Label> name;
+    Box bg;
 };
+
+void addJoystickDisplay(const shared_ptr<input::Joystick>& joystick) {
+    displays.emplace_back(JoystickDisplay({
+        joystick,
+        std::make_shared<text::Label>(joystick->name(),
+                                      text::manager().GetFont("default")),
+        makeBox(Vector2D(1000.0, 100.0), Color(0.1,0.1,0.1))
+    }));
+}
 
 graphic::Canvas& operator<<(graphic::Canvas& canvas, const JoystickDisplay& joy) {
 
+    canvas << joy.bg;
+    canvas.PushAndCompose(math::Geometry(Vector2D(5., 0.)));
     canvas << *joy.name;
+    canvas.PopGeometry();
 
     return canvas;
 }
-
-std::vector<JoystickDisplay> displays;
 
 } // unnamed namespace
 
@@ -121,14 +155,7 @@ int main() {
     // Also, it connects ALL joysticks your OS recognizes. This MIGHT include your keyboard.
     system::FunctionListener<input::JoystickConnectedEvent> joystick_connection_listener(
         [](const auto& ev) {
-            shared_ptr<input::Joystick> joystick = ev.joystick.lock();
-            JoystickDisplay display = {
-                joystick,
-                std::make_shared<text::Label>(joystick->name(),
-                                              text::manager().GetFont("default"))
-            };
-            displays.push_back(display);
-            std::cout << "registered joystick " << joystick->name() << std::endl;
+            addJoystickDisplay(ev.joystick.lock());
         }
     );
     scene->event_handler().AddListener(joystick_connection_listener);
@@ -140,14 +167,14 @@ int main() {
 
     scene->set_render_function([](graphic::Canvas& canvas) {
         using namespace graphic;
-        const Vector2D base(40.0, 40.0);
+        const Vector2D base(10.0, 10.0);
 
         canvas.Clear(ugdk::structure::Color(0.2, 0.2, 0.2, 1));
         canvas.ChangeShaderProgram(graphic::manager().shaders().current_shader());
 
         for (size_t i = 0; i < displays.size(); i++) {
             const JoystickDisplay &display = displays[i];
-            canvas.PushAndCompose(base + i * Vector2D(0.0, 80.0));
+            canvas.PushAndCompose(base + i * Vector2D(0.0, 115.0));
             canvas << display;
             canvas.PopGeometry();
         }
