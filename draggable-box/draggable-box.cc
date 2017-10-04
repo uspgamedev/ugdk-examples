549b6653573e6cfc283fc08b69d4baa68f7617aa
Kazuo256 <kazuo256@gmail.com>
2017-10-04 17:54:03 -0300

    Example adapted to ficticious API for polygon meshes
    Signed off by: @icemage144
    Signed off by: @josealvim

--- draggable-box/draggable-box.cc
+++ draggable-box/draggable-box.cc
@@ -23,16 +23,6 @@ namespace {
 
 const math::Vector2D BOX_SIZE(50.0, 50.0);
 
-struct VertexXYUV {
-    F32 x, y, u, v;
-};
-
-struct Box {
-    math::Vector2D pos;
-    graphic::GLTexture *tex;
-    graphic::VertexData vtx;
-};
-
 }
 
 int main(int argc, char *argv[]) {
@@ -56,51 +46,39 @@ int main(int argc, char *argv[]) {
     scene->event_handler().AddListener(exit_listener);
 
     // Box data
-    Box box = {
-        math::Vector2D(0,0),
-        graphic::manager().white_texture(),
-        std::move(graphic::VertexData(4u, sizeof(VertexXYUV), false))
-    };
-
-    // Map box vertices
-    {
-        graphic::VertexData::Mapper mapper(box.vtx, false);
-        F32 x = static_cast<F32>(BOX_SIZE.x);
-        F32 y = static_cast<F32>(BOX_SIZE.x);
-        mapper.Get<VertexXYUV>(0u) = {.0f, .0f, .0f, .0f};
-        mapper.Get<VertexXYUV>(1u) = {.0f, y,   .0f, .1f};
-        mapper.Get<VertexXYUV>(2u) = {x,   .0f, .1f, .0f};
-        mapper.Get<VertexXYUV>(3u) = {x,   y,   .1f, .1f};
-    }
+    F32 x = static_cast<F32>(BOX_SIZE.x);
+    F32 y = static_cast<F32>(BOX_SIZE.x);
+    Vector2D box_position;
+    graphic::Mesh box = (
+        graphic::DrawMode::TRIANGLE_STRIP(),
+        {
+            .0f, .0f,
+            .0f,   y,
+              x, .0f,
+              x,   y
+        }
+    );
 
     // Box drag event
     system::FunctionListener<input::MouseMotionEvent> box_listener(
-        [&box](const input::MouseMotionEvent& ev) {
-            box.pos.x = static_cast<double>(ev.position.x);
-            box.pos.y = static_cast<double>(ev.position.y);
+        [&box_position](const input::MouseMotionEvent& ev) {
+          box_position.x = static_cast<double>(ev.position.x);
+          box_position.y = static_cast<double>(ev.position.y);
         }
     );
     scene->event_handler().AddListener<input::MouseMotionEvent>(box_listener);
 
     // Rendering
     scene->set_render_function(0u,
-        [&box](graphic::Canvas& canvas) {
-            auto &pos = box.pos;
-            auto &texture = box.tex;
-            auto &vertex_data = box.vtx;
-            
-            /* DOES ALL THE DRAWING */
+        [&box,&box_position](graphic::Canvas& canvas) {
+            auto &pos = box_position;
+
             canvas.Clear(ugdk::structure::Color(0.2, 0.2, 0.2, 1));
-            canvas.ChangeShaderProgram(graphic::manager().shaders().current_shader());
+            //canvas.ChangeShaderProgram(graphic::manager().shaders().current_shader());
             canvas.PushAndCompose(math::Geometry(pos - BOX_SIZE/2));
-            
-            graphic::TextureUnit unit = graphic::manager().ReserveTextureUnit(texture);
-            canvas.SendUniform("drawable_texture", unit);
-            
-            canvas.SendVertexData(vertex_data, graphic::VertexType::VERTEX, 0, 2);
-            canvas.SendVertexData(vertex_data, graphic::VertexType::TEXTURE, 2 * sizeof(F32), 2);
-            canvas.DrawArrays(graphic::DrawMode::TRIANGLE_STRIP(), 0, 4);
-            
+
+            canvas << box;
+
             canvas.PopGeometry();
         });
     
