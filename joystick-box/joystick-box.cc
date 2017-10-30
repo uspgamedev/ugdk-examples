173db4739c00cc39344f0d5be1015d0d0ca6768c
Kazuo256 <kazuo256@gmail.com>
2017-10-30 17:36:13 -0200

    New mesh class in joystick-box

--- joystick-box/joystick-box.cc
+++ joystick-box/joystick-box.cc
@@ -18,10 +18,14 @@
 #include <ugdk/graphic/drawmode.h>
 #include <ugdk/graphic/module.h>
 #include <ugdk/graphic/vertexdata.h>
+#include <ugdk/graphic/mesh.h>
 
 // Type headers
 #include <ugdk/structure/color.h>
 #include <ugdk/structure/types.h>
+
+// GLM headers
+#include <glm/vec2.hpp>
 
 #include <memory>
 #include <iostream>
@@ -30,21 +34,11 @@
 namespace {
 
 using namespace ugdk;
-using math::Vector2D;
+using namespace glm;
 using std::shared_ptr;
 
-const Vector2D  BOX_SIZE(50.0, 50.0);
-const double    SPEED = 500.0;
-
-struct VertexXYUV {
-    F32 x, y, u, v;
-};
-
-struct Box {
-    Vector2D pos;
-    graphic::GLTexture *tex;
-    graphic::VertexData vtx;
-};
+const glm::vec2 BOX_SIZE = { 50.0f, 50.0f };
+const F32       SPEED = 500.0f;
 
 } // unnamed namespace
 
@@ -69,22 +63,16 @@ int main(int argc, char *argv[]) {
     scene->event_handler().AddListener(exit_listener);
 
     // Box data
-    Box box = {
-        math::Vector2D(1280.0, 720.0)/2.0,
-        graphic::manager().white_texture(),
-        std::move(graphic::VertexData(4u, sizeof(VertexXYUV), false))
-    };
-
-    // Map box vertices
-    {
-        graphic::VertexData::Mapper mapper(box.vtx, false);
-        F32 x = static_cast<F32>(BOX_SIZE.x);
-        F32 y = static_cast<F32>(BOX_SIZE.x);
-        mapper.Get<VertexXYUV>(0u) = {.0f, .0f, .0f, .0f};
-        mapper.Get<VertexXYUV>(1u) = {.0f, y,   .0f, .1f};
-        mapper.Get<VertexXYUV>(2u) = {x,   .0f, .1f, .0f};
-        mapper.Get<VertexXYUV>(3u) = {x,   y,   .1f, .1f};
-    }
+    F32 x = static_cast<F32>(BOX_SIZE.x);
+    F32 y = static_cast<F32>(BOX_SIZE.x);
+    vec2 box_position = {1280/2, 720/2};
+    graphic::Mesh2D box(graphic::DrawMode::TRIANGLE_STRIP(), graphic::manager().white_texture());
+    box.Fill({
+        {.0f, .0f, .0f, .0f},
+        {.0f,   y, .0f, 1.f},
+        {  x, .0f, 1.f, 0.f},
+        {  x,   y, 1.f, 1.f}
+    });
 
     action::Scene *scene_ptr = scene.get();
 
@@ -94,14 +82,14 @@ int main(int argc, char *argv[]) {
     // connection logic.
     // Also, it connects ALL joysticks your OS recognizes. This MIGHT include your keyboard.
     system::FunctionListener<input::JoystickConnectedEvent> joystick_connection_listener(
-        [scene_ptr,&box](const auto& ev) {
+        [scene_ptr,&box_position](const auto& ev) {
             shared_ptr<input::Joystick> joystick = ev.joystick.lock();
             std::cout << "registered joystick " << joystick->name() << std::endl;
             // Box move task
-            scene_ptr->AddTask([&box,joystick](double dt) {
+            scene_ptr->AddTask([&box_position,joystick](double dt) {
                 double move_x = joystick->GetAxisStatus(0).Percentage();
                 double move_y = joystick->GetAxisStatus(1).Percentage();
-                box.pos += Vector2D(move_x, move_y)*SPEED*dt;
+                box_position += vec2(move_x, move_y)*SPEED*static_cast<F32>(dt);
             });
         }
     );
@@ -109,21 +97,13 @@ int main(int argc, char *argv[]) {
 
     // Rendering
     scene->set_render_function(0u,
-        [&box](graphic::Canvas& canvas) {
-            auto &pos = box.pos;
-            auto &texture = box.tex;
-            auto &vertex_data = box.vtx;
-            
+        [&box, &box_position](graphic::Canvas& canvas) {
+
             canvas.Clear(ugdk::structure::Color(0.2, 0.2, 0.2, 1));
             canvas.ChangeShaderProgram(graphic::manager().shaders().current_shader());
-            canvas.PushAndCompose(math::Geometry(pos - BOX_SIZE/2));
+            canvas.PushAndCompose(math::Geometry(math::Vector2D(box_position - BOX_SIZE * 0.5f)));
 
-            graphic::TextureUnit unit = graphic::manager().ReserveTextureUnit(texture);
-            canvas.SendUniform("drawable_texture", unit);
-
-            canvas.SendVertexData(vertex_data, graphic::VertexType::VERTEX, 0, 2);
-            canvas.SendVertexData(vertex_data, graphic::VertexType::TEXTURE, 2 * sizeof(F32), 2);
-            canvas.DrawArrays(graphic::DrawMode::TRIANGLE_STRIP(), 0, 4);
+            canvas << box;
 
             canvas.PopGeometry();
         });
