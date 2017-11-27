29a0c497f8448df52868b52d74884476bfa37a20
josealvim <jose.alvim@usp.br>
2017-11-27 18:08:30 -0200

    Change blnkwin to compile and run, with bugs
    It blacks out the whole screen periodically

--- blank-window/blank-window.cc
+++ blank-window/blank-window.cc
@@ -3,9 +3,12 @@
 #include <ugdk/action/scene.h>
 #include <ugdk/input/events.h>
 #include <ugdk/system/compatibility.h>
+
 #include <ugdk/graphic/canvas.h>
+#include <ugdk/graphic/rendertarget.h>
 #include <ugdk/graphic/module.h>
 #include <ugdk/graphic/opengl.h>
+
 #include <ugdk/desktop/module.h>
 #include <ugdk/desktop/window.h>
 #include <ugdk/math/integer2D.h>
@@ -18,26 +21,35 @@ using namespace ugdk;
 
 int main(int argc, char* argv[]) {
     using namespace ugdk;
+    
     system::Configuration config;
-    config.windows_list.front().canvas_size = math::Vector2D(1280, 720);
-    config.windows_list.front().size        = math::Integer2D(1280, 720);
+    auto window_conf = config.windows_list.front();
+    
+    window_conf.canvas_size = math::Vector2D(1280, 720);
+    window_conf.size        = math::Integer2D(1280, 720);
+    
     system::Initialize(config);
+    
+    auto &desktopman = desktop::manager();
+    auto &graphicman = graphic::manager();
+    auto ourscene    = std::make_unique<action::Scene>();
 
+    // We register the screen (associated with the Primary Window) to the graphic manager
+    uint32_t screen_index = graphicman.RegisterScreen(desktopman.primary_window());
+    // And we access the RenderTarget's Renderer and add a step, which is a lambda
+    graphicman.target(screen_index)->MyRenderer()->AddStep(
+        [](graphic::Canvas& canvas) {
+            canvas.Clear(ugdk::structure::Color(0.2, 0.2, 0.2, 1));
+        });
+
+    // This is a listener for the ESC key press event, to quit out.
     system::FunctionListener<input::KeyPressedEvent> listener(
         [] (const ugdk::input::KeyPressedEvent& ev) {
             if (ev.scancode == ugdk::input::Scancode::ESCAPE)
                 ugdk::system::CurrentScene().Finish();
         }
     );
-
-    auto ourscene = std::make_unique<action::Scene>();
-
-    ourscene->event_handler().AddListener(listener);
-    ourscene->set_render_function(0u,
-        [](graphic::Canvas& canvas)
-            {canvas.Clear(ugdk::structure::Color(0.2, 0.2, 0.2, 1));}
-    ); 
-//
+    ourscene->event_handler().AddListener(listener); 
 
     system::PushScene(std::move(ourscene));
 
